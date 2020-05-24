cc85840c4f5da94584e2025780e68d01d4bc363a
Pawan Saini <46812598+pawansaini020@users.noreply.github.com>
2020-05-25 01:56:54 +0530

    Add files via upload

---
+++ simplegridline/FullImageActivity.java
@@ -0,0 +1,28 @@
+package com.example.p.simplegridline;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class FullImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_image);
+
+        Intent i =getIntent();
+
+        int position = i.getExtras().getInt("id");
+        ImageAdapter adapter = new ImageAdapter((this));
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(adapter.images[position]);
+
+
+    }
+
+
+
+}
